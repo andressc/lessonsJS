b7f5b52b1f8c774c86d0c21017e463851f224abf
andrejrebrov <674511674511aaqqww>
2022-07-27 23:49:02 +0300

    Lesson 6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+const account1 = {
+    userName: "Cecil Ireland",
+    transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
+    interest: 1.1,
+    pin: 1111
+};
+
+const account2 = {
+    userName: "Amani Salt",
+    transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
+    interest: 1.3,
+    pin: 2222
+};
+
+const account3 = {
+    userName: "Corey Martinez",
+    transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
+    interest: 0.8,
+    pin: 3333
+};
+
+const account4 = {
+    userName: "Kamile Searle",
+    transactions: [530, 1300, 500, 40, 190],
+    interest: 1,
+    pin: 4444
+};
+
+const account5 = {
+    userName: "Oliver Avila",
+    transactions: [630, 800, 300, 50, 120],
+    interest: 1.1,
+    pin: 5555
+};
+
+const accounts = [account1, account2, account3, account4, account5];
+
 /*const jane = [4, 5, 3, 11, 6, 2, 4, 1, 5, 9];
 const julia = [2, 4, 5, 1, 13, 2, 15, 8, 3, 7];
 const jane2 = [3, 5, 9, 14, 1, 2, 6, 8, 3, 10];
@@ -57,8 +94,16 @@ const arr = transactions.find((v) => v > 0);
 console.log(arr);*/
 
 //lesson 5
-const arr = [[1, 3, 5], [4, 6], [1, 2], 7, 8, 9];
-console.log(arr.flat());
+/*const arr = [[1, 3, 5], [4, 6], [1, 2], 7, 8, 9];
+console.log(arr.flat());*/
 
+//lesson 6
+const arr = accounts
+    .map((v) => v.transactions)
+    .flat()
+    .reduce((r, v) => r + v, 0);
 
+const arr2 = accounts.flatMap((v) => v.transactions).reduce((r, v) => r + v, 0);
 
+console.log(arr);
+console.log(arr2);
